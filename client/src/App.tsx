7c6cebba94fdc4a6a83dd34872c22f482d6426d5
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-10-06 08:51:27 +0900

    graphコンポーネント化

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,89 +1,12 @@
-import React, { useEffect } from 'react';
-import socketIOClient from 'socket.io-client';
-import Chart from 'react-apexcharts';
+import React from 'react';
 import './App.css';
-import { useAppSelector, useAppDispatch } from './app/hooks'
-import {selectGraphData, addGraphData} from './features/graphDataSlice'
 
+import Graph from './components/Graph'
 
-const ENDPOINT = ':3001';
 const App: React.FC = () => {
-  const { graphData } = useAppSelector(selectGraphData)
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.on('FromAPI', (data: number) => {
-      let dt = new Date()
-      let hour = dt.getHours()
-      let minute = dt.getMinutes()
-      let second = dt.getSeconds()
-      let date = `${hour}:${minute}:${second}`
-      dispatch(addGraphData({ x: date, y: Number(data) }))
-    });
-  }, [dispatch]);
-
-  const state = {
-    options: {
-      chart: {
-        id: 'realtime',
-        animations: {
-          enabled: true,
-          dynamicAnimation: {
-            speed: 1000,
-          },
-        },
-        toolbar: {
-          show: false,
-        },
-        zoom: {
-          enabled: false,
-        },
-        dataLabels: {
-          enabled: false,
-        },
-        stroke: {
-          curve: 'stepline',
-        },
-        title: {
-          text: 'Dynamic Updating Chart',
-          align: 'left',
-        },
-        markers: {
-          size: 0,
-        },
-        xaxis: {
-          type: 'datetime',
-          range: 10000,
-        },
-        yaxis: {
-          max: 100,
-        },
-        legend: {
-          show: false,
-        },
-      },
-      xaxis: {
-        categories: graphData.x,
-      },
-    },
-    series: [
-      {
-        data: graphData.y,
-      },
-    ],
-  };
-
   return (
     <div className="App">
-      <h1>アクティブユーザー{graphData.y.slice(-1)[0]}</h1>
-      <Chart
-        options={state.options}
-        series={state.series}
-        type="line"
-        width={500}
-        height={200}
-      />
+      <Graph />
     </div>
   );
 };
